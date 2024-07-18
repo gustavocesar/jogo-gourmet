b2610e36c8a69bdc163af2b9a4c15a1075d645c2
Gustavo César <Gustavo Csar>
2024-07-18 17:17:55 -0300

    Estrutura de dados (árvore binária)

---
+++ src/EstruturaDeDados/ArvoreBinaria.cs
@@ -0,0 +1,16 @@
+﻿namespace JogoGourmet.EstruturaDeDados;
+
+public class ArvoreBinaria
+{
+    public ArvoreBinaria()
+    {
+        //inicialização com dois pratos básicos
+        Esquerda = new No("Lasanha");
+        Direita = new No("Bolo de Chocolate");
+        Raiz = new No("Massa", Esquerda, Direita);
+    }
+
+    public No Raiz { get; private set; }
+    public No Esquerda { get; private set; }
+    public No Direita { get; private set; }
+}
---
+++ src/EstruturaDeDados/No.cs
@@ -0,0 +1,29 @@
+﻿namespace JogoGourmet.EstruturaDeDados;
+
+public class No
+{
+    public No(string valor)
+    {
+        Valor = valor;
+    }
+
+    public No(string valor, No? esquerda, No? direita)
+    {
+        Valor = valor;
+        Esquerda = esquerda;
+        Direita = direita;
+    }
+
+    public string Valor { get; private set; }
+    public No? Esquerda { get; private set; }
+    public No? Direita { get; private set; }
+
+    public bool IsNoFolha() =>
+        Esquerda is null && Direita is null;
+
+    public void SetEsquerda(No esquerda) =>
+        Esquerda = esquerda;
+
+    public void SetDireita(No direita) =>
+        Direita = direita;
+}
