5922eccd124f0c72a1802770c8679c9c586b62f4
Gustavo César <Gustavo Csar>
2024-07-18 18:55:15 -0300

    Testes: classe Jogo

---
+++ tests/JogoTests.cs
@@ -0,0 +1,17 @@
+﻿using JogoGourmet.Interface;
+using Moq;
+using Xunit;
+
+namespace JogoGourmet.Tests;
+
+public class JogoTests
+{
+    [Fact]
+    public void Jogo_InicializaComDoisPratosBasicos()
+    {
+        var mockInteracaoUsuario = new Mock<IInterfaceComUsuario>();
+        var jogo = new Jogo(mockInteracaoUsuario.Object);
+
+        Assert.NotNull(jogo);
+    }
+}
